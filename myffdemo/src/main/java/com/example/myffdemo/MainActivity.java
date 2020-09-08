2e7d7121786675f31b0a06ef466b606bd8830852
luyanhao <luyanhao@xiaomi.com>
2020-09-08 17:49:39 +0800

    为 MyFFDemo 添加 native-lib.cpp 并成功调用

--- myffdemo/src/main/java/com/example/myffdemo/MainActivity.java
+++ myffdemo/src/main/java/com/example/myffdemo/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.myffdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NativeLib native_lib = new NativeLib();
+
+        TextView text_view = (TextView) findViewById(R.id.text_view);
+        text_view.setText(native_lib.stringFromJNI());
     }
+
 }
---
+++ myffdemo/src/main/java/com/example/myffdemo/NativeLib.java
@@ -0,0 +1,11 @@
+package com.example.myffdemo;
+
+/**
+ * Created by luyanhao 20-9-8.
+ */
+public class NativeLib {
+    static {
+        System.loadLibrary("native-lib");
+    }
+    public native String stringFromJNI();
+}
